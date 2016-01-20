d3bc34f45e39d042b37049cbbdb3e8a969317be3
Mitch Comardo <comamitc@gmail.com>
2016-01-19 18:38:13 -0600

    adding client

---
+++ project.clj
@@ -0,0 +1,41 @@
+(defproject quarto "0.1.0-SNAPSHOT"
+  :description "Multiplayer game of Quarto"
+  :url "https://github.com/comamitc/quarto"
+  :license {
+    :name "MIT License"
+    :url "https://github.com/comamitc/quarto/blob/master/LICENSE"
+    :distribution :repo}
+
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.7.228"]
+                 [org.clojure/core.async "0.2.374"]]
+
+  :plugins [[lein-cljsbuild "1.1.2"]]
+
+  :source-paths ["src"]
+
+  :cljsbuild { 
+    :builds {
+      :client {
+        :source-paths ["src/client"]
+        :compiler {
+          :output-to "public/js/client.js"
+          :output-dir "public/out"
+          :optimizations :whitespace}}
+
+      :client-adv {
+        :source-paths ["src/client"]
+        :compiler {
+          ;; :externs ["externs/jquery-1.9.js" "externs/socket.io.js"]
+          :output-to "public/js/client.min.js"
+          :optimizations :advanced
+          :pretty-print false}}
+
+     :server {
+      :source-paths ["src/server"]
+      :compiler {
+        :language-in :ecmascript5
+        :language-out :ecmascript5
+        :target :nodejs
+        :output-to "server.js"
+        :optimizations :simple}}}})
---
+++ src/client/core.cljs
@@ -0,0 +1,27 @@
+(ns client.core)
+
+(def board (atom [0 0 0 0
+                  0 0 0 0
+                  0 0 0 0
+                  0 0 0 0]))
+
+;; :size 0 (small) 1 (large)
+;; :color 0 (black) 1 (white)
+;; :shape 0 (circle) 1 (square)
+;; :hole 0 (solid) 1 (hallow)
+(def pieces (atom [{:size 0 :color 0 :shape 0 :hole 0}
+                   {:size 0 :color 0 :shape 0 :hole 1}
+                   {:size 0 :color 0 :shape 1 :hole 1}
+                   {:size 0 :color 1 :shape 0 :hole 1}
+                   {:size 0 :color 1 :shape 1 :hole 1}
+                   {:size 1 :color 0 :shape 0 :hole 1}
+                   {:size 1 :color 1 :shape 0 :hole 1}
+                   {:size 1 :color 0 :shape 1 :hole 1}
+                   {:size 1 :color 1 :shape 1 :hole 1}
+                   {:size 1 :color 1 :shape 1 :hole 0}
+                   {:size 1 :color 1 :shape 0 :hole 0}
+                   {:size 1 :color 0 :shape 1 :hole 0}
+                   {:size 1 :color 1 :shape 1 :hole 0}
+                   {:size 1 :color 0 :shape 0 :hole 0}
+                   {:size 1 :color 1 :shape 0 :hole 0}
+                   {:size 1 :color 0 :shape 0 :hole 0}]))
